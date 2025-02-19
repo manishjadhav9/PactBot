16d194fa730d70647b7be4f2c5f18583ebbb97dd
manishjadhav9 <manishsj289@gmail.com>
2025-02-20 00:28:46 +0530

    created user model and auth route

--- server/src/app.ts
+++ server/src/app.ts
@@ -5,6 +5,11 @@ import cors from "cors";
 import helmet from "helmet";
 import morgan from "morgan";
 import mongoose from "mongoose";
+import passport from "passport";
+import "./config/passport";
+import MongoStore from "connect-mongo";
+import session from "express-session";
+import authRoute from "./routes/auth";
 
 const app = express();
 
@@ -25,6 +30,25 @@ app.use(morgan("dev"));
 
 
 app.use(express.json());
+
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET!,
+    resave: false,
+    saveUninitialized: false,
+    store: MongoStore.create({ mongoUrl: process.env.MONGODB_URI! }),
+    cookie: {
+      secure: process.env.NODE_ENV === "production",
+      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+      maxAge: 24 * 60 * 60 * 1000, // 1 day
+    },
+  })
+);
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/auth", authRoute);
 
 const PORT = 8080;
 app.listen(PORT, () => {
